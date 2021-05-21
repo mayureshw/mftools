e7c0452da1d616dd915b2ddbe4df92ba130f5e63
mayureshw <mayureshw@github>
2021-05-21 20:44:34 +0530

    cagr added at total level

--- pfviews.py
+++ pfviews.py
@@ -10,15 +10,18 @@ class PFViews(PFData):
     @lru_cache(maxsize=1)
     def cost(self): return sum(o.cost() for o in self.pfobjs)
     def gain(self): return self.value() - self.cost()
+    def cagr(self): return sum(o.cost()*o.cagr() for o,po in self.buymatches())/self.cost()
     def pshare(self,l): return [ (p,v,v*100/self.value()) for p,v in l ]
     def aggr(self,p): return [ (p,sum(o.value() for o in os)) for p,os in self.by(p) ]
     def pfgainreport(self):
         fp = open('pfgainreport.txt','w')
-        printTbl([[ 'EQ' if o.iseq else 'DT', o.isfree, po.shortf(), o.units, o.cost(), o.value(), o.value()-o.cost(),
-            ((o.value()-o.cost())*100/o.cost()) if o.cost() else '-', o.cagr(), str(po.rating()), po.oyret(), po.subcat() ]
+        printTbl([[ 'EQ' if o.iseq else 'DT', o.isfree, po.shortf(), o.units, o.cost(), o.value(),
+            o.value()-o.cost(), ((o.value()-o.cost())*100/o.cost()) if o.cost() else '-', o.cagr(),
+            str(po.rating()), po.oyret(), po.subcat() ]
             for o,po in self.buymatches()],
             title = 'PFGAIN',
-            colnames = ['Typ','Free','Fund','Units','Cost','Value','Gain','%Gain','CAGR','Rat','1YRet','Subcat'],
+            colnames = ['Typ','Free','Fund','Units','Cost','Value','Gain','%Gain','CAGR','Rat','1YRet',
+                'Subcat'],
             sort = [-1,-2,-9,10],
             formaters = {4:'%7.2f',5:'%8.0f',6:'%8.0f',7:'%8.0f',8:'%5.2f',9:'%5.2f',11:'%5.2f',12:'%-11s'},
             file=fp
@@ -27,10 +30,10 @@ class PFViews(PFData):
     def pfreport(self):
         fp = open('pfreport.txt','w')
         print('\n',file=fp)
-        printTbl([[self.value(),self.cost(),self.gain()]],
+        printTbl([[self.cost(),self.value(),self.gain(),self.gain()*100/self.cost(),self.cagr()]],
             title = 'TOTAL VALUE',
-            colnames = ['Value','Cost','Gain'],
-            formaters = {1:'%8.0f',2:'%8.0f',3:'%8.0f'},
+            colnames = ['Cost','Value','Gain','%Gain','CAGR'],
+            formaters = {1:'%8.0f',2:'%8.0f',3:'%8.0f',4:'%5.2f',5:'%5.2f'},
             file=fp,
             )
         print('\n',file=fp)
