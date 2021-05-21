39d70d78760d64696072cdd594ec74a0f382aaaa
mayureshw <mayureshw@github>
2021-05-21 22:29:35 +0530

    purchase date added, sorting modified

--- pfviews.py
+++ pfviews.py
@@ -15,15 +15,28 @@ class PFViews(PFData):
     def aggr(self,p): return [ (p,sum(o.value() for o in os)) for p,os in self.by(p) ]
     def pfgainreport(self):
         fp = open('pfgainreport.txt','w')
-        printTbl([[ 'EQ' if o.iseq else 'DT', o.isfree, po.shortf(), o.units, o.cost(), o.value(),
-            o.value()-o.cost(), ((o.value()-o.cost())*100/o.cost()) if o.cost() else '-', o.cagr(),
-            str(po.rating()), po.oyret(), po.subcat() ]
+        printTbl([[
+            'EQ' if o.iseq else 'DT', #1
+            o.isfree, #2
+            po.shortf(), #3
+            o.bt.txndt.strftime('%y%m%d'), #4
+            o.units, #5
+            o.cost(), #6
+            o.value(), #7
+            o.value() - o.cost(), #8
+            ((o.value()-o.cost())*100/o.cost()) if o.cost() else '-', #9
+            o.cagr(), #10
+            str(po.rating()), #11
+            po.oyret(), #12
+            po.subcat(), #13
+            ]
             for o,po in self.buymatches()],
             title = 'PFGAIN',
-            colnames = ['Typ','Free','Fund','Units','Cost','Value','Gain','%Gain','CAGR','Rat','1YRet',
-                'Subcat'],
-            sort = [-1,-2,-9,10],
-            formaters = {4:'%7.2f',5:'%8.0f',6:'%8.0f',7:'%8.0f',8:'%5.2f',9:'%5.2f',11:'%5.2f',12:'%-11s'},
+            colnames = ['Typ','Free','Fund','Date','Units','Cost','Value','Gain','%Gain','CAGR','Rat',
+                '1YRet','Subcat'],
+            sort = [-1,-2,11,3],
+            formaters = {5:'%9.4f',6:'%8.0f',7:'%8.0f',8:'%8.0f',9:'%5.2f',10:'%5.2f',12:'%5.2f',
+                13:'%-11s'},
             file=fp
             )
         fp.close()
